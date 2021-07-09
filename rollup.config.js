4528906bddb9eebce4e1710f11ee3fd9e557b7dc
dgbarclay <dan@danbarclay.com>
2021-07-09 13:56:19 +0100

    Remove ts dependency

--- rollup.config.js
+++ rollup.config.js
@@ -20,10 +20,9 @@
  */
 
 import pkg from "./package.json";
-import typescript from "rollup-plugin-typescript2";
 
 export default {
-  input: "./src/index.ts",
+  input: "./src/index.js",
   output: [
     {
       file: pkg.main,
@@ -46,16 +45,6 @@ export default {
       name: "SolidClient",
     },
   ],
-  plugins: [
-    typescript({
-      // Use our own version of TypeScript, rather than the one bundled with the plugin:
-      typescript: require("typescript"),
-      tsconfigOverride: {
-        compilerOptions: {
-          module: "esnext",
-        },
-      },
-    }),
-  ],
+  plugins: [],
   external: [],
 };
