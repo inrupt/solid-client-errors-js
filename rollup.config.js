6920ed4a5ada06c8af7b0fc61d4443210df06314
dgbarclay <dan@danbarclay.com>
2021-07-22 12:48:10 +0100

    Add typescript plugin

--- rollup.config.js
+++ rollup.config.js
@@ -19,32 +19,43 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import pkg from "./package.json";
-
-export default {
-  input: "./src/index.js",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-    },
-    {
-      file: pkg.module,
-      entryFileNames: "[name].es.js",
-      format: "esm",
-    },
-    {
-      dir: "dist",
-      entryFileNames: "[name].mjs",
-      format: "esm",
-      preserveModules: true,
-    },
-    {
-      dir: "umd",
-      format: "umd",
-      name: "SolidClient",
-    },
-  ],
-  plugins: [],
-  external: [],
-};
+ import pkg from "./package.json";
+ import typescript from "rollup-plugin-typescript2";
+ 
+ export default {
+   input: "./src/index.ts",
+   output: [
+     {
+       file: pkg.main,
+       format: "cjs",
+     },
+     {
+       file: pkg.module,
+       entryFileNames: "[name].es.js",
+       format: "esm",
+     },
+     {
+       dir: "dist",
+       entryFileNames: "[name].mjs",
+       format: "esm",
+       preserveModules: true,
+     },
+     {
+       dir: "umd",
+       format: "umd",
+       name: "SolidClient",
+     },
+   ],
+   plugins: [
+     typescript({
+       // Use our own version of TypeScript, rather than the one bundled with the plugin:
+       typescript: require("typescript"),
+       tsconfigOverride: {
+         compilerOptions: {
+           module: "esnext",
+         },
+       },
+     }),
+   ],
+   external: [],
+ };
