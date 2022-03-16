677487251c2112ce41102b43d5be59816a349405
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-03-16 17:33:16 +0100

    Fix package name

--- .github/workflows/cd-packaging-tests/node/esmodule.mjs
+++ .github/workflows/cd-packaging-tests/node/esmodule.mjs
@@ -1,7 +1,7 @@
 // Verify that imports from the main export work:
-import { sampleModuleFn as mainModuleFn } from "@inrupt/solid-client-errors-js";
+import { sampleModuleFn as mainModuleFn } from "@inrupt/solid-client-errors";
 // Verify that submodule imports work:
-import sampleModuleFn from "@inrupt/solid-client-errors-js/module";
+import sampleModuleFn from "@inrupt/solid-client-errors/module";
 
 console.log(mainModuleFn());
 console.log(sampleModuleFn());
