d3e2cb2c1acf7c13d0e8c5ac76a6839a5830c17e
Pete Edwards <edwardsph@users.noreply.github.com>
2022-06-16 11:24:13 +0100

    Run Sonar analysis with test coverage on PRs

--- jest.config.js
+++ jest.config.js
@@ -24,6 +24,7 @@ module.exports = {
   testEnvironment: "jsdom",
   clearMocks: true,
   collectCoverage: true,
+  coverageReporters: ["text", "lcov"],
   coverageThreshold: {
     global: {
       branches: 100,
