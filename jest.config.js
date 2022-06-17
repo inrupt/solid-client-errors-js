b62d62794b3a894307f4df696cf4c203a664f09c
Pete Edwards <edwardsph@users.noreply.github.com>
2022-06-17 10:23:20 +0100

    Integrate Sonar scan into main CI

--- jest.config.js
+++ jest.config.js
@@ -24,7 +24,7 @@ module.exports = {
   testEnvironment: "jsdom",
   clearMocks: true,
   collectCoverage: true,
-  coverageReporters: ["text", "lcov"],
+  coverageReporters: process.env.CI ? ["text", "lcov"] : ["text"],
   coverageThreshold: {
     global: {
       branches: 100,
